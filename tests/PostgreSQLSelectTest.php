eb61f93aece9c4e8f7c32e3718cc607a355bfa84
Michael Collette <metrol@metrol.net>
2016-03-23 09:44:07 -0500

    New tests for sub selects in the FROM and WHERE clauses.

--- tests/PostgreSQLSelectTest.php
+++ tests/PostgreSQLSelectTest.php
@@ -153,4 +153,163 @@ SQL;
 
         $this->assertEquals($expected, $select->output());
     }
+
+    /**
+     * Test the ability of using one SELECT statement to reference a sub-SELECT
+     * in the FROM clause.  Also tests that the sub-SELECT's bindings are passed
+     * up to the parent properly.
+     *
+     */
+    public function testSubSelectInFrom()
+    {
+        $select = DBSql::PostgreSQL()->select();
+        $sub    = DBSql::PostgreSQL()->select();
+
+        $sub->field('description')
+            ->from('relatedData')
+            ->where('id = ?', [86]);
+
+        $select->fromSub('reldtq', $sub);
+
+        // Fetch bindings from the parent SELECT.  The sub select should have
+        // merged with the parent.
+        $bindings = $select->getBindings();
+
+        $this->assertCount(1, $bindings);
+        $this->assertContains(86, $bindings);
+
+        $label = key($bindings);
+
+        $expected = <<<SQL
+SELECT
+    *
+FROM
+    (
+        SELECT
+            "description"
+        FROM
+            "relatedData"
+        WHERE
+            "id" = {$label}
+    ) "reldtq"
+
+SQL;
+
+        $this->assertEquals($expected, $select->output());
+    }
+
+    /**
+     * See if the sub select works properly in a WHERE IN statement.  To keep
+     * things interesting, mix that in with other criteria in the WHERE
+     * statement.
+     *
+     */
+    public function testSubSelectInWhere()
+    {
+        $select = DBSql::PostgreSQL()->select();
+        $sub    = DBSql::PostgreSQL()->select();
+
+        $sub->field('description')
+            ->from('relatedData')
+            ->where('id = ?', [86]);
+
+        $select->field('*')
+            ->from('tableWithData twd')
+            ->where('twd.value = ?', [42])
+            ->whereInSub('twd.description', $sub)
+            ->where('twd.id < ?', [97]);
+
+        $bindings = $select->getBindings();
+        $actual   = $select->output();
+
+        $this->assertCount(3, $bindings);
+        $this->assertContains(86, $bindings);
+        $this->assertContains(42, $bindings);
+        $this->assertContains(97, $bindings);
+
+        $label1 = array_search(86, $bindings);
+        $label2 = array_search(42, $bindings);
+        $label3 = array_search(97, $bindings);
+
+        $expected = <<<SQL
+SELECT
+    *
+FROM
+    "tableWithData" "twd"
+WHERE
+    "twd"."value" = {$label2}
+    AND
+    "twd"."description" IN
+    (
+        SELECT
+            "description"
+        FROM
+            "relatedData"
+        WHERE
+            "id" = {$label1}
+    )
+    AND
+    "twd"."id" < {$label3}
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * See if the sub select works properly in a WHERE NOT IN statement.
+     *
+     */
+    public function testSubSelectNotInWhere()
+    {
+        $select = DBSql::PostgreSQL()->select();
+        $sub    = DBSql::PostgreSQL()->select();
+
+        $sub->field('description')
+            ->from('relatedData')
+            ->where('id = ?', [86]);
+
+        $select->field('*')
+            ->from('tableWithData twd')
+            ->where('twd.value = ?', [42])
+            ->whereNotInSub('twd.description', $sub)
+            ->where('twd.id < ?', [97]);
+
+        $bindings = $select->getBindings();
+        $actual   = $select->output();
+
+        $this->assertCount(3, $bindings);
+        $this->assertContains(86, $bindings);
+        $this->assertContains(42, $bindings);
+        $this->assertContains(97, $bindings);
+
+        $label1 = array_search(86, $bindings);
+        $label2 = array_search(42, $bindings);
+        $label3 = array_search(97, $bindings);
+
+        $expected = <<<SQL
+SELECT
+    *
+FROM
+    "tableWithData" "twd"
+WHERE
+    "twd"."value" = {$label2}
+    AND
+    "twd"."description" NOT IN
+    (
+        SELECT
+            "description"
+        FROM
+            "relatedData"
+        WHERE
+            "id" = {$label1}
+    )
+    AND
+    "twd"."id" < {$label3}
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+    }
 }
+
