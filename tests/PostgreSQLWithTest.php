4f9ce38297f7b72481c2733fb86b3929203cf947
Michael Collette <metrol@metrol.net>
2016-03-24 20:25:08 -0500

    Add testing for recursive With statements.

--- tests/PostgreSQLWithTest.php
+++ tests/PostgreSQLWithTest.php
@@ -73,4 +73,265 @@ FROM
 SQL;
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * PostgreSQL has a little tweak to the With clause, allowing for Recursive
+     * calls.  This sample SQL was modeled after the example in the PostgreSQL
+     * manual
+     *
+     */
+    public function testRecursiveSelect()
+    {
+        $union = DBSql::PostgreSQL()->union();
+        $sel1  = DBSql::PostgreSQL()->select();
+        $sel2  = DBSql::PostgreSQL()->select();
+        $suff  = DBSql::PostgreSQL()->select();
+
+        $recursFields = ['sub_part', 'part', 'quantity'];
+
+        $sel1->fields(['sub_part', 'part', 'quantity'])
+            ->from('parts')
+            ->where('part = ?', ['our_product'])
+        ;
+
+        $sel2->fields(['p.sub_part', 'p.part', 'p.quantity'])
+            ->from('included_parts pr')
+            ->from('parts p')
+            ->where('p.part = pr.sub_part');
+
+        $suff->field('sub_part')
+            ->enableQuoting(false)
+            ->field('SUM(quantity) total_quantity')
+            ->enableQuoting(true)
+            ->from('included_parts')
+            ->groupBy('sub_part');
+
+        $union->setSelect($sel1)
+            ->setSelect($sel2, $union::UNION_ALL);
+
+        // print PHP_EOL.$sel1->output();
+        // print PHP_EOL.$sel2->output().'X';
+        // print PHP_EOL.$suff->output();
+        // print PHP_EOL.$union->output().'X';
+
+        // First pass, just make sure that all the queries so far are solid
+        $actual   = $sel1->output();
+        $label    = key($sel1->getBindings());
+        $expected = <<<SQL
+SELECT
+    "sub_part",
+    "part",
+    "quantity"
+FROM
+    "parts"
+WHERE
+    "part" = {$label}
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+
+        $actual   = $sel2->output();
+        $expected = <<<SQL
+SELECT
+    "p"."sub_part",
+    "p"."part",
+    "p"."quantity"
+FROM
+    "included_parts" "pr",
+    "parts" "p"
+WHERE
+    "p"."part" = "pr"."sub_part"
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+
+        $actual   = $suff->output();
+        $expected = <<<SQL
+SELECT
+    "sub_part",
+    SUM(quantity) total_quantity
+FROM
+    "included_parts"
+GROUP BY
+    "sub_part"
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+
+        $actual   = $union->output();
+        $label    = key($union->getBindings());
+        $expected = <<<SQL
+SELECT
+    "sub_part",
+    "part",
+    "quantity"
+FROM
+    "parts"
+WHERE
+    "part" = {$label}
+
+UNION ALL
+
+SELECT
+    "p"."sub_part",
+    "p"."part",
+    "p"."quantity"
+FROM
+    "included_parts" "pr",
+    "parts" "p"
+WHERE
+    "p"."part" = "pr"."sub_part"
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+
+        // Now to start with just adding all the statements together without
+        // the recursive ability.
+        $with  = DBSql::PostgreSQL()->with();
+
+        $with->setStatement('parts_is_parts', $sel1)
+            ->setStatement('which_parts', $sel2)
+            ->setSuffix($suff);
+
+        $actual = $with->output();
+        $label  = key($with->getBindings());
+        $expected = <<<SQL
+WITH
+"parts_is_parts" AS 
+(
+    SELECT
+        "sub_part",
+        "part",
+        "quantity"
+    FROM
+        "parts"
+    WHERE
+        "part" = {$label}
+),
+"which_parts" AS 
+(
+    SELECT
+        "p"."sub_part",
+        "p"."part",
+        "p"."quantity"
+    FROM
+        "included_parts" "pr",
+        "parts" "p"
+    WHERE
+        "p"."part" = "pr"."sub_part"
+)
+SELECT
+    "sub_part",
+    SUM(quantity) total_quantity
+FROM
+    "included_parts"
+GROUP BY
+    "sub_part"
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+        $this->assertCount(1, $with->getBindings());
+        $this->assertContains('our_product', $with->getBindings());
+
+        // Next up, put together a With statement using the union of the first
+        // two select statements.  No fields specified.
+        $with  = DBSql::PostgreSQL()->with();
+
+        $with->setRecursive('included_parts', $union)
+             ->setSuffix($suff);
+
+        $actual = $with->output();
+        $label  = key($with->getBindings());
+
+        $expected = <<<SQL
+WITH RECURSIVE "included_parts" AS 
+(
+    SELECT
+        "sub_part",
+        "part",
+        "quantity"
+    FROM
+        "parts"
+    WHERE
+        "part" = {$label}
+    
+    UNION ALL
+    
+    SELECT
+        "p"."sub_part",
+        "p"."part",
+        "p"."quantity"
+    FROM
+        "included_parts" "pr",
+        "parts" "p"
+    WHERE
+        "p"."part" = "pr"."sub_part"
+)
+SELECT
+    "sub_part",
+    SUM(quantity) total_quantity
+FROM
+    "included_parts"
+GROUP BY
+    "sub_part"
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+        $this->assertCount(1, $with->getBindings());
+        $this->assertContains('our_product', $with->getBindings());
+
+        // Next up, put together a With statement using the union of the first
+        // two select statements.  This time with fields
+        $with  = DBSql::PostgreSQL()->with();
+
+        $with->setRecursive('included_parts', $union, $recursFields)
+             ->setSuffix($suff);
+
+        $actual = $with->output();
+        $label  = key($with->getBindings());
+
+        $expected = <<<SQL
+WITH RECURSIVE "included_parts"("sub_part", "part", "quantity") AS 
+(
+    SELECT
+        "sub_part",
+        "part",
+        "quantity"
+    FROM
+        "parts"
+    WHERE
+        "part" = {$label}
+    
+    UNION ALL
+    
+    SELECT
+        "p"."sub_part",
+        "p"."part",
+        "p"."quantity"
+    FROM
+        "included_parts" "pr",
+        "parts" "p"
+    WHERE
+        "p"."part" = "pr"."sub_part"
+)
+SELECT
+    "sub_part",
+    SUM(quantity) total_quantity
+FROM
+    "included_parts"
+GROUP BY
+    "sub_part"
+
+SQL;
+
+        $this->assertEquals($expected, $actual);
+        $this->assertCount(1, $with->getBindings());
+        $this->assertContains('our_product', $with->getBindings());
+    }
 }
