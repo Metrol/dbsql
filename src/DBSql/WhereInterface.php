129865505f121064e161d177804f7e01f1629d06
Michael Collette <metrol@metrol.net>
2016-08-16 13:51:03 -0500

    Create a new interface to define a Where clause
    This will be used to specify both the parent WHERE as well as the more
    specific types of where clauses.
    
    issue #1

---
+++ src/DBSql/WhereInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author        Michael Collette <mcollette@meetingevolution.net>
+ * @version       1.0
+ * @package       Metrol/DBSql
+ * @copyright (c) 2016, Michael Collette
+ */
+
+namespace Metrol\DBSql;
+
+/**
+ * Describes objects that provide where clauses
+ *
+ */
+interface WhereInterface
+{
+    /**
+     * Produce the text of the clause
+     *
+     * @return string
+     */
+    public function output();
+
+    /**
+     * Fetch any bindings that may have been used
+     *
+     * @return array
+     */
+    public function getBindings();
+}
