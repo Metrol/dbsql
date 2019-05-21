72769968ab92ba9d26369ded37621d38baaf62b5
Michael Collette <metrol@metrol.net>
2019-05-21 11:57:20 -0500

    Update the Insert Interface to reflect the changes to the class

--- src/DBSql/InsertInterface.php
+++ src/DBSql/InsertInterface.php
@@ -24,26 +24,6 @@ interface InsertInterface extends StatementInterface
     public function table($tableName);
 
     /**
-     * Add a set of the field names to show up in the INSERT statement.
-     * No value binding provided.
-     *
-     * @param string[] $fields
-     *
-     * @return $this
-     */
-    public function fields(array $fields);
-
-    /**
-     * Add a set of the values to assign to the INSERT statement.
-     * No value binding provided.  No automatic quoting.
-     *
-     * @param array $values
-     *
-     * @return $this
-     */
-    public function values(array $values);
-
-    /**
      * Sets a SELECT statement that will be used as the source of data for the
      * INSERT.  Any values that have been set will be ignored.  Any bindings
      * from the Select statement will be merged.
@@ -54,16 +34,14 @@ interface InsertInterface extends StatementInterface
      */
     public function valueSelect(SelectInterface $select);
 
-
     /**
-     * Add a set of fields with values to the select request.
-     * Values automatically create bindings.
+     * Add a Field Value to the stack
      *
-     * @param array $fieldValues
+     * @param Field\Value $fieldValue
      *
      * @return $this
      */
-    public function fieldValues(array $fieldValues);
+    public function addFieldValue(Field\Value $fieldValue);
 
     /**
      * Add a field to return back from the insert statement.  Accepts either a
